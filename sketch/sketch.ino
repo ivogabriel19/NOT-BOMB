f27b36c3abedd7367b51e0e200aa70ec38d3f4ef
ivogabriel19 <ivodegange@gmail.com>
2023-08-22 02:19:55 -0300

    2AM me fui a dormir, hacer que cuente con 4 digitos

--- sketch/sketch.ino
+++ sketch/sketch.ino
@@ -1,32 +1,29 @@
 /* Proyecto para el scape room de captacion
  * First Approach: Temporizador en 7 segmentos
- *                 Arduino UNO con display 7 segmentos CA
+ *                 NodeMCU 1.0 v2 con display 7 segmentos CA
 */
 
-#define a 16 //D0
-#define b  5 //D1
-#define c  4 //D2
-#define d  0 //D3
-#define e  2 //D4
-#define f 14 //D5
-#define g 12 //D6
-#define p 13 //D7
+#define a 2//D4
+#define b 0//D3
+#define c 4//D2
+#define d 5//D1
+#define seg 16 //D0
 
 #define INTERVAL 1000
 
-const int pines[] = {a, b, c, d, e, f, g, p};
-const bool common = true; // True = CA | False = CC
-const int digitos[][7] = {
-    {1,1,1,1,1,1,0}, //0
-    {0,1,1,0,0,0,0}, //1
-    {1,1,0,1,1,0,1}, //2
-    {1,1,1,1,0,0,1}, //3
-    {0,1,1,0,0,1,1}, //4
-    {1,0,1,1,0,1,1}, //5
-    {1,0,1,1,1,1,1}, //6
-    {1,1,1,0,0,0,0}, //7
-    {1,1,1,1,1,1,1}, //8
-    {1,1,1,1,0,1,1}  //9
+const int pines[] = {d, c, b, a};
+//const bool common = true; // True = CA | False = CC
+const int bcd[][4] = {
+    {0,0,0,0}, //0
+    {0,0,0,1}, //1
+    {0,0,1,0}, //2
+    {0,0,1,1}, //3
+    {0,1,0,0}, //4
+    {0,1,0,1}, //5
+    {0,1,1,0}, //6
+    {0,1,1,1}, //7
+    {1,0,0,0}, //8
+    {1,0,0,1}  //9
   };
 
 void setup() {
@@ -34,12 +31,7 @@ void setup() {
   for(int i = 0; i < (sizeof(pines)/sizeof(pines[0])) ; i++){
     pinMode(pines[i], OUTPUT);
     }
-
-  for(int i = 0; i < (sizeof(pines)/sizeof(pines[0])) ; i++){
-    digitalWrite(pines[i], HIGH);
-    delay(300);
-    digitalWrite(pines[i], LOW);
-    }
+  pinMode(seg ,OUTPUT);
 }
 
 void loop() {
@@ -59,16 +51,16 @@ void segundero(){
 
   // blink segundero
   static bool LEDstate = 0;
-  digitalWrite(p, LEDstate);
+  digitalWrite(seg, LEDstate);
   LEDstate = !LEDstate;
 
   Serial.print("Numero " + String(num) + ": {");
-  for (int pin=0 ; pin < (sizeof(pines)/sizeof(pines[0]))-1 ; pin++){
-    digitalWrite(pines[pin], digitos[num][pin] ^ common);
-    //Serial.print(String(digitos[num][pines[pin]]) + ", ");
+  for (int pin=0 ; pin < (sizeof(pines)/sizeof(pines[0])) ; pin++){
+    digitalWrite(pines[pin], bcd[num][pin]);
+    Serial.print(String(bcd[num][pin]) + ", ");
     }
   Serial.println("}");
-   
+
   num++;
   if(num>9) num=0;
   //Serial.println("blink_segundero() " + String(num));
