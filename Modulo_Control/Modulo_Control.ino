ca14b9f194a4fd25e9c22ef68d8de9b523c6e430
ivogabriel19 <ivodegange@gmail.com>
2023-08-24 20:01:16 -0300

    Codigo ESP32

--- Modulo_Control/Modulo_Control.ino
+++ Modulo_Control/Modulo_Control.ino
@@ -3,18 +3,22 @@
  *                 ESP32 DevKit
 */
 
-#define a 16
-#define b 17
-#define c 18
-#define d 19
-#define t1 22
-#define t2 23
+#define dA 16
+#define dB 17
+#define dC 18
+#define dD 19
+
+#define anA 21
+#define anB 22
+#define anC 23
+
 #define seg 2 //Built in LED
 
 #define INTERVAL 1000
 
-const int pines[] = {d, c, b, a};
-const int bcd[][4] = {
+const int pinesDig[] = {dD, dC, dB, dA};
+const int pinesAn[] = {anA, anB, anC};
+const int bcdDig[][4] = {
     {0,0,0,0}, //0
     {0,0,0,1}, //1
     {0,0,1,0}, //2
@@ -26,23 +30,30 @@ const int bcd[][4] = {
     {1,0,0,0}, //8
     {1,0,0,1}  //9
   };
+const int bcdAn[][3] = {
+  {0, 0, 0},
+  {0, 0, 1},
+  {0, 1, 0}
+};
 
 void setup() {
   Serial.begin(115200);
-  for(int i = 0; i < (sizeof(pines)/sizeof(pines[0])) ; i++){
-    pinMode(pines[i], OUTPUT);
+  for(int i = 0; i < (sizeof(pinesDig)/sizeof(pinesDig[0])) ; i++){
+    pinMode(pinesDig[i], OUTPUT);
     }
   pinMode(seg ,OUTPUT);
   pinMode(t1 ,OUTPUT);
   pinMode(t2 ,OUTPUT);
 
   digitalWrite(t1, LOW);
-  digitalWrite(t2, LOW);
+  digitalWrite(t2, HIGH  );
 }
 
 void loop() {
   unsigned static long previousMillis = 0;    // will store last time DHT was updated
   unsigned long currentMillis = millis();
+  static int dig1 = 0;
+  static int dig2 = 0;
 
   //Segundero
   if (currentMillis - previousMillis >= INTERVAL) {
@@ -50,26 +61,46 @@ void loop() {
     previousMillis = currentMillis;
     segundero();
   }
+
+  //Escribir displays
+  //Escribo primer digito
+  digitalWrite(anA, LOW);
+  digitalWrite(anB, LOW);
+  digitalWrite(anC, LOW);
+  Serial.print("Numero " + String(dig1) + ": {");
+
+  for (int pin=0 ; pin < (sizeof(pinesDig)/sizeof(pinesDig[0])) ; pin++){
+    digitalWrite(pinesDig[pin], bcdDig[dig1][pin]);
+    Serial.print(String(bcdDig[dig1][pin]) + ", ");
+    }
+  
+  Serial.println("}");
+  delay(100);
+
+  
+  //Escribo primer segundo
+  digitalWrite(anA, HIGH);
+  digitalWrite(anB, LOW);
+  digitalWrite(anC, LOW);
+  Serial.print("Numero " + String(dig2) + ": {");
+
+  for (int pin=0 ; pin < (sizeof(pinesDig)/sizeof(pinesDig[0])) ; pin++){
+    digitalWrite(pinesDig[pin], bcdDig[dig2][pin]);
+    Serial.print(String(bcdDig[dig2][pin]) + ", ");
+    }
+  
+  Serial.println("}");
+  delay(100);
 }
 
 void segundero(){
-  static int num = 0;
 
   // blink segundero
   static bool LEDstate = 0;
   digitalWrite(seg, LEDstate);
   LEDstate = !LEDstate;
 
-  Serial.print("Numero " + String(num) + ": {");
-
-  for (int pin=0 ; pin < (sizeof(pines)/sizeof(pines[0])) ; pin++){
-    digitalWrite(pines[pin], bcd[num][pin]);
-    Serial.print(String(bcd[num][pin]) + ", ");
-    }
-  
-  Serial.println("}");
-
-  num++;
-  if(num>9) num=0;
+  dig1++;
+  if(dig1>9) dig1=0;
   //Serial.println("blink_segundero() " + String(num));
   }
