6d8f4225ec1d3d18d17ceedf6e070b5ff6a46bda
ivogabriel19 <ivodegange@gmail.com>
2023-08-24 22:34:57 -0300

    Segundero funcionando y escalable

--- Modulo_Control/Modulo_Control.ino
+++ Modulo_Control/Modulo_Control.ino
@@ -3,21 +3,26 @@
  *                 ESP32 DevKit
 */
 
+//pines escritura segmentos
 #define dA 16
 #define dB 17
 #define dC 18
 #define dD 19
 
+//pines escritura anodos segmentos
 #define anA 21
 #define anB 22
 #define anC 23
 
 #define seg 2 //Built in LED
 
-#define INTERVAL 1000
+int ctrlSegundero = 1000; //Variable control actualizacion del segundero
 
-const int pinesDig[] = {dD, dC, dB, dA};
-const int pinesAn[] = {anA, anB, anC};
+
+int contador[] = {0, 0}; // numeros del reloj -> {unidadSegundos , decenaSegundos}
+
+const int pinesDig[] = {dD, dC, dB, dA};  //pines escritura segmentos
+const int pinesAn[] = {anC, anB, anA};    //pines escritura anodos segmentos
 const int bcdDig[][4] = {
     {0,0,0,0}, //0
     {0,0,0,1}, //1
@@ -41,66 +46,68 @@ void setup() {
   for(int i = 0; i < (sizeof(pinesDig)/sizeof(pinesDig[0])) ; i++){
     pinMode(pinesDig[i], OUTPUT);
     }
+  for(int i = 0; i < (sizeof(pinesAn)/sizeof(pinesAn[0])) ; i++){
+  pinMode(pinesAn[i], OUTPUT);
+  }
   pinMode(seg ,OUTPUT);
-  pinMode(t1 ,OUTPUT);
-  pinMode(t2 ,OUTPUT);
-
-  digitalWrite(t1, LOW);
-  digitalWrite(t2, HIGH  );
 }
 
 void loop() {
-  unsigned static long previousMillis = 0;    // will store last time DHT was updated
+  unsigned static long previousMillis = 0;
   unsigned long currentMillis = millis();
-  static int dig1 = 0;
-  static int dig2 = 0;
 
   //Segundero
-  if (currentMillis - previousMillis >= INTERVAL) {
-    // save the last time you updated the flag values
+  if (currentMillis - previousMillis >= ctrlSegundero) {
     previousMillis = currentMillis;
     segundero();
   }
 
-  //Escribir displays
-  //Escribo primer digito
-  digitalWrite(anA, LOW);
-  digitalWrite(anB, LOW);
-  digitalWrite(anC, LOW);
-  Serial.print("Numero " + String(dig1) + ": {");
-
-  for (int pin=0 ; pin < (sizeof(pinesDig)/sizeof(pinesDig[0])) ; pin++){
-    digitalWrite(pinesDig[pin], bcdDig[dig1][pin]);
-    Serial.print(String(bcdDig[dig1][pin]) + ", ");
-    }
-  
-  Serial.println("}");
-  delay(100);
-
-  
-  //Escribo primer segundo
-  digitalWrite(anA, HIGH);
-  digitalWrite(anB, LOW);
-  digitalWrite(anC, LOW);
-  Serial.print("Numero " + String(dig2) + ": {");
-
-  for (int pin=0 ; pin < (sizeof(pinesDig)/sizeof(pinesDig[0])) ; pin++){
-    digitalWrite(pinesDig[pin], bcdDig[dig2][pin]);
-    Serial.print(String(bcdDig[dig2][pin]) + ", ");
-    }
-  
-  Serial.println("}");
-  delay(100);
+  refresh();
 }
 
 void segundero(){
+
+  Serial.println("Segundero: " + String(ctrlSegundero));
+  ctrlSegundero-=50;
+  if(ctrlSegundero <= 0) ctrlSegundero = 50;
 
   // blink segundero
   static bool LEDstate = 0;
   digitalWrite(seg, LEDstate);
   LEDstate = !LEDstate;
 
-  dig1++;
-  if(dig1>9) dig1=0;
+  //incremento reloj
+  contador[0]++;
+  if(contador[0]>9) {
+    contador[0]=0;
+    contador[1]++;
+    if(contador[1]>9){
+      contador[1]=0;
+      }
+    }
   //Serial.println("blink_segundero() " + String(num));
   }
+
+void refresh(){//Escribir displays
+  //Escribir cada digito
+  for(int dig=0 ; dig < 2 ; dig++){
+    //Alimento el anodo correspondiente
+    digitalWrite(anA, bcdAn[dig][2]);
+    digitalWrite(anB, bcdAn[dig][1]);
+    digitalWrite(anC, bcdAn[dig][0]);
+ 
+    //escribo los segmentos correspondientes
+    //Serial.print("Cuenta:" + String(dig2) + String(dig1) + " Digito 1 Numero " + String(dig1) + ": {");
+    for (int pin=0 ; pin < (sizeof(pinesDig)/sizeof(pinesDig[0])) ; pin++){
+      digitalWrite(pinesDig[pin], bcdDig[contador[dig]][pin]);
+      //Serial.print(String(bcdDig[dig1][pin]) + ", ");
+      }
+    //Serial.println("}");
+    
+    //Apago los segmentos
+    delay(1);
+    for (int pin=0 ; pin < (sizeof(pinesDig)/sizeof(pinesDig[0])) ; pin++){
+      digitalWrite(pinesDig[pin], 1);
+      }
+   }  
+}
